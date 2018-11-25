a0a33d7931efadd6822d651dcd88bdef4786fc12
uniquexiaobai <unique_xiaobai@qq.com>
2018-11-25 20:25:00 +0800

    insertionSort with binarySearch

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -9,7 +9,30 @@ def insertionSort(xs):
         xs[j + 1] = x
     return xs
 
-xs = [1, 4, 9, 2, 7, 5]
+def instertionSortWithBinarySearch(xs):
+	n = len(xs)
+	for i in range(1, n):
+		x = xs[i]
+		p = binarySearch(xs[:i], x)
+		for j in range(i, p, -1):
+			xs[j] = xs[j - 1]
+		xs[p] = x
+	return xs;
 
+def binarySearch(xs, x):
+	l = 0
+	r = len(xs) - 1
+	while (l <= r):
+		mid = (l + r) // 2
+		if x == xs[mid]:
+			return mid
+		elif x > xs[mid]:
+			l = mid + 1
+		else:
+			r = mid - 1
+	return l
+
+xs = [1, 4, 9, 2, 7, 5]
 print(xs)
 print(insertionSort(xs[:]))
+print(instertionSortWithBinarySearch(xs))
